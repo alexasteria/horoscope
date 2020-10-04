d2618028cb6c2108f1b3ba5652fd25bbf4cebc84
Alex <alexasteria@icloud.com>
2020-10-04 04:36:54 +0300

    Updates

---
+++ precache-manifest.fed77d61fae35e2791438c4606063107.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc535f7e8a13e35fb9503367e395452",
+    "url": "/horoscope/index.html"
+  },
+  {
+    "revision": "7a1582e361c1b0e4bf84",
+    "url": "/horoscope/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "a1dcce3bfb94004e93d7",
+    "url": "/horoscope/static/css/main.fde9a153.chunk.css"
+  },
+  {
+    "revision": "7a1582e361c1b0e4bf84",
+    "url": "/horoscope/static/js/2.a3230ac3.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/horoscope/static/js/2.a3230ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1dcce3bfb94004e93d7",
+    "url": "/horoscope/static/js/main.e2e620b5.chunk.js"
+  },
+  {
+    "revision": "baea82dfd04f550fab65",
+    "url": "/horoscope/static/js/runtime-main.0ac36cb9.js"
+  },
+  {
+    "revision": "915d84b75121417ee745eaa47b695af4",
+    "url": "/horoscope/static/media/background.915d84b7.jpeg"
+  }
+]);
